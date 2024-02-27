ef8890dcf9f83fe9110240eb5472dc10e062e375
MoatazNoaman <moataz.noaman12@gmail.com>
2024-02-27 22:47:23 +0200

    first implement 0.01

--- app/src/main/java/com/example/d_id_videostream/domain/useCases/PeerConnectionObserver.kt
+++ app/src/main/java/com/example/d_id_videostream/domain/useCases/PeerConnectionObserver.kt
@@ -1,37 +1,47 @@
 package com.example.d_id_videostream.domain.useCases
 
+import android.util.Log
 import org.webrtc.DataChannel
 import org.webrtc.IceCandidate
 import org.webrtc.MediaStream
 import org.webrtc.PeerConnection
 import org.webrtc.RtpReceiver
 
+private const val TAG = "PeerConnectionObserver"
 class PeerConnectionObserver: PeerConnection.Observer {
     override fun onSignalingChange(p0: PeerConnection.SignalingState?) {
+        Log.d(TAG, "onSignalingChange: ${p0?.toString()}")
     }
 
     override fun onIceConnectionChange(p0: PeerConnection.IceConnectionState?) {
+        Log.d(TAG, "onIceConnectionChange: ${p0?.name}")
     }
 
     override fun onIceConnectionReceivingChange(p0: Boolean) {
+        Log.d(TAG, "onIceConnectionReceivingChange: ${p0}")
     }
 
     override fun onIceGatheringChange(p0: PeerConnection.IceGatheringState?) {
+        Log.d(TAG, "onIceGatheringChange: ${p0?.name}")
     }
 
     override fun onIceCandidate(p0: IceCandidate?) {
+        Log.d(TAG, "onIceCandidate: ${p0.toString()}")
     }
 
     override fun onIceCandidatesRemoved(p0: Array<out IceCandidate>?) {
+        Log.d(TAG, "onIceCandidatesRemoved: ${p0?.joinToString()}")
     }
 
     override fun onAddStream(p0: MediaStream?) {
+        Log.d(TAG, "onAddStream: ${p0.toString()}")
     }
 
     override fun onRemoveStream(p0: MediaStream?) {
     }
 
     override fun onDataChannel(p0: DataChannel?) {
+        Log.d(TAG, "onDataChannel: ${p0.toString()}")
     }
 
     override fun onRenegotiationNeeded() {
--- app/src/main/java/com/example/d_id_videostream/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/d_id_videostream/presentation/ui/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.d_id_videostream.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import com.example.d_id_videostream.data.repo.Resource
 import com.example.d_id_videostream.databinding.ActivityMainBinding
 import com.example.d_id_videostream.domain.useCases.PeerConnectionObserver
 import com.example.d_id_videostream.presentation.viewModel.ActivityViewModelImpl
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
@@ -18,6 +21,7 @@ import org.webrtc.SdpObserver
 import org.webrtc.SessionDescription
 import java.lang.IllegalArgumentException
 
+private const val TAG = "MainActivity"
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -47,26 +51,27 @@ class MainActivity : AppCompatActivity() {
                     }
 
                     is Resource.Success -> {
-                        binding.label.text = it.data.iceServers.filter {
-                            it.urls is String
-                        }.map {
-                            if (it.urls is List<*>) PeerConnection.IceServer.builder(it.urls[0].toString())
-                                .createIceServer() else PeerConnection.IceServer.builder(it.urls.toString())
-                                .createIceServer()
-                        }.toList()[0].toString()
-//                        val peerConnection = createPeerConnection(
-//                            SessionDescription(SessionDescription.Type.OFFER, it.data.offer.sdp),
-//                            it.data.iceServers.filter {
-//                                it.urls is String
-//                            }.map {
-//                                if (it.urls is List<*>) PeerConnection.IceServer.builder(it.urls[0].toString())
-//                                    .createIceServer() else PeerConnection.IceServer.builder(it.urls.toString())
-//                                    .createIceServer()
-//                            }.toList()
-//                        )
-//                        binding.label.setText("Fetched Successfully: ${peerConnection.localDescription}")
+                        binding.label.text = it.data.id + "\n" + it.data.iceServers.joinToString()
+                        val stunUrl = it.data.iceServers.filter { it.urls is String }[0].urls as String
+                        val turnUrls = it.data.iceServers.filter { it.urls is List<*> }.flatMap {
+                            it.urls as List<String>
+                        }
+                        Log.d(TAG, "onCreate: stunurl: $stunUrl")
+                        Log.d(TAG, "onCreate: turnUrls: $turnUrls")
+
+                        val iceServers = mutableListOf<PeerConnection.IceServer>()
+
+                        // Create ICE server objects
+                        iceServers.add(PeerConnection.IceServer.builder(stunUrl).createIceServer())
+                        turnUrls.forEach { url ->
+                            iceServers.add(PeerConnection.IceServer.builder(url).createIceServer())
+                        }
 
 
+                        CoroutineScope(Dispatchers.Unconfined).launch {
+                            val peerConnection = createPeerConnection(SessionDescription(SessionDescription.Type.OFFER, it.data.offer.sdp), iceServers)
+                            Log.d(TAG, "onCreate: ${peerConnection?.localDescription?.description}")
+                        }
                     }
                 }
             }
@@ -76,18 +81,20 @@ class MainActivity : AppCompatActivity() {
     suspend fun createPeerConnection(
         offer: SessionDescription,
         iceServers: List<PeerConnection.IceServer>
-    ): PeerConnection {
-        val peerConnection: PeerConnection = createPeerConnectionInternal(iceServers)
+    ): PeerConnection? {
+        val peerConnection: PeerConnection = createPeerConnectionInternal(iceServers) ?: return null
         peerConnection.setRemoteDescription(object : SdpObserver {
             override fun onCreateSuccess(p0: SessionDescription?) {
-
+                Log.d(TAG, "onCreateSuccess: remote desc is created")
             }
 
             override fun onSetSuccess() {
+                Log.d(TAG, "onCreateSuccess: remote desc is set successfully")
                 peerConnection.createAnswer(object : SdpObserver {
                     override fun onCreateSuccess(sessionDescription: SessionDescription) {
                         peerConnection.setLocalDescription(object : SdpObserver {
                             override fun onCreateSuccess(p0: SessionDescription?) {
+                                Log.d(TAG, "onCreateSuccess: ${p0?.description}")
                             }
 
                             override fun onSetSuccess() {
@@ -125,12 +132,16 @@ class MainActivity : AppCompatActivity() {
         return peerConnection
     }
 
-    private fun createPeerConnectionInternal(iceServers: List<PeerConnection.IceServer>): PeerConnection {
+    private fun createPeerConnectionInternal(iceServers: List<PeerConnection.IceServer>): PeerConnection? {
 
-        val peerConnectionFactory = PeerConnectionFactory.builder().createPeerConnectionFactory()
-        val peerConnection = peerConnectionFactory.createPeerConnection(iceServers, PeerConnectionObserver())
-        if (peerConnection == null)
-            throw IllegalArgumentException("peer failed to create: " + iceServers.joinToString())
-        return peerConnection
+       return try{
+           val peerConnectionFactory = PeerConnectionFactory.builder().createPeerConnectionFactory()
+           val rtc_connection = PeerConnection.RTCConfiguration(iceServers)
+           val peerConnection = peerConnectionFactory.createPeerConnection(rtc_connection, PeerConnectionObserver())
+           peerConnection
+       }catch (E:Exception){
+           Log.d(TAG, "createPeerConnectionInternal: ${E.message}")
+           return null
+       }
     }
 }
